ce5f333d331815d427d352009da8dc1e91e87e63
= <=>
2020-08-02 12:28:28 +0530

    .NET exception handling using try,catch and finally

---
+++ NET Exception Handling/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Exampl.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("File not found!");
+                Console.WriteLine("Caught exception {0}",ex);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Directory not found");
+                Console.WriteLine(@"Caught exception C:\Lesson22 existiert : {0}",ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception caught");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Closing application now . . .");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
